ccc418d500252263e5ca73c202a989dae1e15689
BrianFleming <brian88@outlook.com>
2023-03-22 09:03:22 +0000

    added address and oreder

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ app.config['SECRET_KEY'] = '8BYkEfBA6O6briaWlSihBXox7C0sKR6b'
 
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///kitty.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///kittySales.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -49,11 +49,11 @@ class User(UserMixin, db.Model):
     name = db.Column(db.String(250), nullable=False)
     password = db.Column(db.String(250), nullable=False)
     email = db.Column(db.String(250), unique=True, nullable=False)
-    posts = relationship("KittyPost", back_populates="author")
+    posts = relationship("KittyItem", back_populates="author")
 
 
-class KittyPost(db.Model):
-    __tablename__ = "kitty_posts"
+class KittyItem(db.Model):
+    __tablename__ = "kitty_item"
     id = db.Column(db.Integer, primary_key=True)
     # Foreign Key to link to the user's post
     author_id = db.Column(db.Integer, db.ForeignKey("Users.id"))
@@ -63,30 +63,60 @@ class KittyPost(db.Model):
     body = db.Column(db.Text, nullable=False)
     img_url = db.Column(db.String(250), nullable=False)
 
+
+class Address(db.Model):
+    __tablename__ = "Address"
+    id = db.Column(db.Integer, primary_key=True)
+    address1 = db.Column(db.String(100), nullable=False)
+    address2 = db.Column(db.String(100), nullable=False)
+    address3 = db.Column(db.String(100), nullable=False)
+    post_code = db.Column(db.String(50), nullable=False)
+    country = db.Column(db.String(50), nullable=False)
+    user = db.Column(db.Integer, db.ForeignKey("User.id"))
+
+
+class boughtBy(db.Model):
+    __tablename__ = "sales"
+    id = db.Column(db.Integer, primary_key=True)
+    user = relationship("User", back_populates='name')
+    email = relationship("User", back_populates='email')
+    item = relationship("KittyItem", back_populates='title')
+    date = db.Column(db.String(50), nullable=False)
+
+
 # db.create_all()
 
 
 # Create form for registration of new user
-# class RegisterForm(FlaskForm):
-#     name = StringField("Name", validators=[DataRequired()])
-#     email = StringField("Email", validators=[DataRequired()])
-#     password = StringField("Password", validators=[DataRequired()])
-#     submit = SubmitField("Sign me up!")
+class RegisterForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    password = StringField("Password", validators=[DataRequired()])
+    submit = SubmitField("Sign me up!")
 
 
-# class UserLogin(FlaskForm):
-#     email = StringField("Email", validators=[DataRequired()])
-#     password = StringField("Password", validators=[DataRequired()])
-#     submit = SubmitField("Log me in!")
+class UserLogin(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = StringField("Password", validators=[DataRequired()])
+    submit = SubmitField("Log me in!")
 
 
-class CreatePostForm(FlaskForm):
+class CreateItemForm(FlaskForm):
     title = StringField("Title", validators=[DataRequired()])
     description = StringField("Description", validators=[DataRequired()])
     img_url = StringField("Image URL", validators=[DataRequired()])
     author = StringField("Author", validators=[DataRequired()])
     body = CKEditorField("Item Content", validators=[DataRequired()])
     submit = SubmitField("Update new item")
+
+
+class CreateAddressForm(FlaskForm):
+    address1 = StringField("Address1", validators=[DataRequired()])
+    address2 = StringField("Address2", validators=[DataRequired()])
+    address3 = StringField("Address3")
+    post_code = StringField("PostCode / Zip", validators=[DataRequired()])
+    country = StringField("Country")
+    submit = SubmitField("Enter address")
 
 
 @login_manager.user_loader
@@ -115,6 +145,16 @@ def generic():
 @app.route("/add-post")
 def add_new_item():
     return render_template("add-post.html")
+
+
+@app.route("/basket")
+def basket():
+    return render_template("basket.html")
+
+
+@app.route("/payment")
+def payment():
+    return render_template("payment.html")
 
 
 if __name__ == "__main__":
